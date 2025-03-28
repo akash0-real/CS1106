59185dd8b5b45438de5763cafb517bdb93c23348
Akash bisht <aayush45r@gmail.com>
2025-03-28 09:48:30 +0530

    Update a1.sh

--- lab5/a1.sh
+++ lab5/a1.sh
@@ -1,7 +1,7 @@
 mkdir -p /temp/backup
 cp *.c *.py /temp/backup
 cd /temp/
-tar -czvf /temp/backup.tar.gz -C /temp backup
+tar -czvf /temp/backup.tar.gz 
 udisksctl mount -b /dev/sda1
 mv /temp/backup.tar.gz /media/rvu/pendrive/
 udisksctl umount -b /dev/sda1
